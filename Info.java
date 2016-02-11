657225709572b2a2cf78836af269b1a353faeac3
sabrina <sad.angel8@hotmail.fr>
2016-02-11 16:41:53 +0100

    creation de la fonction Info

---
+++ Info.java
@@ -0,0 +1,15 @@
+package com.vishesh.eight_puzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Info extends Activity  {
+    //Called when the activity is first created
+       @Override
+           public void onCreate(Bundle savedInstanceState) {
+	           super.onCreate(savedInstanceState);
+		           setContentView(R.layout.info);
+			       }
+			       }
+
