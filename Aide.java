6b1af019db244cdf22b9de0b764653d7f48b3edf
sabrina <sad.angel8@hotmail.fr>
2016-02-11 16:29:30 +0100

    creation de la fonctione Aide

---
+++ Aide.java
@@ -0,0 +1,14 @@
+package com.vishesh.eight_puzzle;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class Aide extends Activity  {
+    //Appeller lorsque l'activitè est crèe pour la premiere fois
+        @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	            super.onCreate(savedInstanceState);
+		            setContentView(R.layout.aide);
+			        }
+				}
