6c0c681f9df7f0c76ca624ea3d9b96cfc9fc3b76
meziane <meziane.msm@gmail.com>
2016-02-11 13:52:41 +0100

    creation du main

--- main.java
+++ main.java
@@ -1,1 +1,52 @@
+import android.content.Intent;
 
+
+import android.os.Bundle;
+
+
+import android.view.Menu;
+
+
+import android.view.MenuInflater;
+
+
+import android.view.MenuItem;
+
+
+import android.view.View;
+
+
+import android.view.View.OnClickListener;
+
+
+import android.widget.Button;
+
+
+
+public class main extends Activity {
+
+ 
+ @Override
+        
+	 
+public void onCreate(Bundle savedInstanceState) {
+	 	       
+		        
+super.onCreate(savedInstanceState);
+					 
+setContentView(R.layout.main);
+					  	
+
+Button jouer=(Button) findViewById(R.id.jouer);
+								
+jouer.setOnClickListener(new OnClickListener() {
+
+
+// apelle de la vue du premier niveau
+										
+@Override
+Intent intent = new Intent(getApplicationContext(), JeuxN1.class);
+startActivity(intent);
+}});
+}
+																						 							
